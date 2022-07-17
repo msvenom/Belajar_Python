1c6bda9eae112a215f16c934eef0850642d096d7
msfvenom <109444551+msvenom@users.noreply.github.com>
2022-07-17 09:27:25 +0700

    Create Variabel.py

---
+++ Variabel.py
@@ -0,0 +1,8 @@
+# A variable is a place to store data
+# To create a variable, use the = . sign
+
+Name_Variabel = "Text Or String"
+
+# To display the data in a variable, use the variable name
+
+print(Name_Variabel)
